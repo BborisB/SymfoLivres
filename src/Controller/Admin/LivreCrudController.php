481be191cd29c4e9697930cb3d72e834190bb170
Boris Bétrémieux <b.boris.2001@gmail.com>
2023-12-07 16:20:29 +0100

    livres

--- src/Controller/Admin/LivreCrudController.php
+++ src/Controller/Admin/LivreCrudController.php
@@ -5,7 +5,9 @@ namespace App\Controller\Admin;
 use App\Entity\Livre;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class LivreCrudController extends AbstractCrudController
@@ -18,10 +20,12 @@ class LivreCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
+            IdField::new('id')->hideOnForm(),
             TextField::new('titre'),
             AssociationField::new('auteur'),
             AssociationField::new('editeur'),
-            ImageField::new('imageName', 'Image de couverture')->setUploadDir("public/assets/images")->setBasePath("assets/images")
+            ImageField::new('imageName', 'Image de couverture')->setUploadDir("public/assets/images")->setBasePath("assets/images"),
+            NumberField::new('quantite')
         ];
     }
 }
